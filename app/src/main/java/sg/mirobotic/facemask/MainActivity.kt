a7f8c471ea52cdccb8bea6736e57a7542f28409e
Imran Khan <iamkhan007@outlook.com>
2021-06-03 11:52:30 +0530

    first commit

---
+++ app/src/main/java/sg/mirobotic/facemask/MainActivity.kt
@@ -0,0 +1,13 @@
+package sg.mirobotic.facemask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
